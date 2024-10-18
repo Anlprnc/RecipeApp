d9cf0fcd48d02645422686595ff7da88e0b13b04
Anlprnc <anilpirincciii@gmail.com>
2024-10-18 12:39:22 +0300

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Anıl on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
